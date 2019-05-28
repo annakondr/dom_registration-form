c50df973c13a0f29d0e9b87ee455803407fb45ed
annakondr <tyukondra@gmail.com>
2019-05-28 16:59:00 +0300

    next_try

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-'use strict';
+//'use strict';
 const body = document.querySelector('body');
 const nameInput = document.querySelector('.name');
 const phoneNumber = document.querySelector('.number');
 const submitButton = document.querySelector('.submit');
-const nameValidationRE = /([a-z]*\s[a-z]*((\s([a-z]*))?)$)/i;
+const nameValidationRE = /^([a-z]*\s[a-z]*((\s([a-z]*))?)$)/i;
 const numberValidationRE = /(\+380)?(380)?0[- ]?\d\d[- ]?\d\d\d[- ]?\d\d[- ]?\d\d$/;
 const checkboxInfo = document.querySelector('.checkbox');
 const phoneRegionInfo = document.querySelector('.phone-region');
@@ -12,12 +12,12 @@ const regionSelection = document.querySelector('.region');
 nameInput.addEventListener('change', validateName);
 function validateName() {
     let valueName = nameInput.value.trim();
-    if (nameValidationRE.test(valueName) ) {
-        nameInput.classList.add('valid');
-        nameInput.classList.remove('invalid');
+    if (!nameValidationRE.test(valueName) ) {
+        nameInput.classList.add('invalid');
+        nameInput.classList.remove('valid');
         return false
     }
-    nameInput.classList.remove('valid')
+    nameInput.classList.add('valid')
 }
 
 phoneNumber.addEventListener('change',validateNumber);
@@ -36,65 +36,50 @@ function hideInfo () {
     phoneRegionInfo.hidden = !phoneRegionInfo.hidden;
 }
 
-const center = document.querySelector('.home-city-center');
-const north = document.querySelector('.home-city-north');
-const east = document.querySelector('.home-city-east');
-const south = document.querySelector('.home-city-south');
-const west = document.querySelector('.home-city-west');
-const lableHomeCity = document.querySelector('.lableHomeCity');
+const homeCityList = {
+    Center: ['Select home city', 'Cherkasy', 'Dnipro', 'Kropyvnytskyi', 'Poltava', 'Vinnytsia', 'Zhytomyr', 'Not in the list'],
+    North: ['Select home city', 'Chernihiv', 'Sumy', 'Not in the list'],
+    East: ['Select home city', 'Donetsk', 'Kharkiv', 'Luhansk', 'Not in the list'],
+    South: ['Select home city', 'Kherson', 'Mykolaiv', 'Odesa', 'Zaporizhzhia', 'Not in the list'],
+    West: ['Select home city', 'Chernivtsi', 'Ivano-Frankivsk', 'Khmelnytskyi', 'Lutsk', 'Lviv', 'Rivne', 'Ternopil', 'Uzhhorod', 'Not in the list']
+};
 
+const form = document.querySelector('.form');
+const selectCity = document.querySelector('.select-home');
+const selectHomeCity = document.querySelector('.select-homecity')
+selectHomeCity.classList.add('invisible')
 regionSelection.addEventListener('change', showCityList);
 function showCityList() {
-    if (regionSelection.value === "Center") {
-        lableHomeCity.classList.remove('invisible');
-        center.classList.remove('invisible')
-    } else {
-        center.classList.add('invisible');
-        lableHomeCity.classList.add('invisible')
+    const citiesList = homeCityList[regionSelection.value];
+
+    while (selectCity.hasChildNodes()) {
+        selectCity.removeChild(selectCity.firstChild);
     }
-    if (regionSelection.value === "North") {
-        lableHomeCity.classList.remove('invisible');
-        north.classList.remove('invisible')
+
+    if (citiesList) {
+        selectHomeCity.classList.remove('invisible');
+        citiesList.forEach(option =>
+            selectCity.insertAdjacentHTML('beforeend', `<option>${option}</option>>`))
+        selectCity.firstChild.selected = true
+        selectCity.firstChild.disabled = true
     } else {
-        north.classList.add('invisible');
-        lableHomeCity.classList.add('invisible')
-    }
-    if (regionSelection.value === "East") {
-        lableHomeCity.classList.remove('invisible');
-        east.classList.remove('invisible')
-    } else {
-        east.classList.add('invisible');
-        lableHomeCity.classList.add('invisible')
-    }
-    if (regionSelection.value === "South") {
-        lableHomeCity.classList.remove('invisible');
-        south.classList.remove('invisible')
-    } else {
-        south.classList.add('invisible');
-        lableHomeCity.classList.add('invisible')
-    }
-    if (regionSelection.value === "West") {
-        lableHomeCity.classList.remove('invisible');
-        west.classList.remove('invisible')
-    } else {
-        west.classList.add('invisible');
-        lableHomeCity.classList.add('invisible')
+        selectHomeCity.classList.add('invisible');
     }
 }
 
-const form = document.querySelector('.form');
 form.addEventListener('input', changeButton);
 submitButton.disabled = true;
 function changeButton() {
-    if (checkboxInfo.checked) {
-        if (nameInput.className === 'name valid'){
-            submitButton.disabled = false;
-        }
-    } else {
-        if (nameInput.className === 'name valid' && phoneNumber.className === 'number valid'){
-            submitButton.disabled = false;
-        }
-    }
+    const validName = nameInput.classList.contains('valid');
+    const validNumber = phoneNumber.classList.contains('valid');
     submitButton.disabled = true;
+    if (checkboxInfo.checked) {
+        submitButton.disabled = !validName;
+    } else {
+        submitButton.disabled = true;
+        if (validName && validNumber && regionSelection.options[1]) {
+            submitButton.disabled = false;
+        } else
+            submitButton.disabled = !(validName && validNumber && regionSelection.options[regionSelection.selectedIndex].value && selectCity.options[selectCity.selectedIndex]);
+    }
 }
-
